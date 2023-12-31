f3267622b5e1c7042b2ae89ee7dd21cb77086585
aykutcayir34 <aykut.cayir@khas.edu.tr>
2023-12-31 19:43:05 +0300

    Model loadfromptretrained is added.

--- mamba.py
+++ mamba.py
@@ -48,6 +48,36 @@ class Mamba(nn.Module):
             x = layer(x)
         x = self.norm(x)
         return self.lm_head(x)
+    
+    @staticmethod
+    def from_pretrained(model_name: str):
+        from transformers.utils import WEIGHTS_NAME, CONFIG_NAME
+        from transformers.utils import cached_file
+        def load_config_hf(path_to_config_file):
+            resolved_archive_file = cached_file(model_name, CONFIG_NAME, _raise_exceptions_for_missing_entries=False)
+            return json.load(open(resolved_archive_file))
+        
+        def load_state_dict_hf(model_name, device=None, dtypes=None):
+            resolved_archive_file = cached_file(model_name, WEIGHTS_NAME, _raise_exceptions_for_missing_entries=False)
+            state_dict = torch.load(resolved_archive_file, weights_only=True, map_location=device, mmap=True)
+            return state_dict
+
+        config_file = load_config_hf(model_name)
+        args = ModelArgs(
+            d_model=config_file["d_model"],
+            n_layers=config_file["n_layer"],
+            vocab_size=config_file["vocab_size"],
+        )
+        model= Mamba(args)
+        state_dict = load_state_dict_hf(model_name)
+        new_state_dict = {}
+        for key in state_dict:
+            new_key = key.replace("backbone.", "")
+            new_state_dict[new_key] = state_dict[key]
+
+        model.load_state_dict(new_state_dict)
+        return model
+
 
 class MambaBlock(nn.Module):
     pass
