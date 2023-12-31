6a892f86b723e75a8f6f33aba062a30173ec7684
aykutcayir34 <aykut.cayir@khas.edu.tr>
2023-12-31 20:28:18 +0300

    completed

--- mamba.py
+++ mamba.py
@@ -37,7 +37,7 @@ class Mamba(nn.Module):
         super().__init__()
         self.args = args
         self.embedding = nn.Embedding(args.vocab_size, args.d_model)
-        self.layers = nn.ModuleList([MambaBlock(args) for _ in range(args.n_layers)])  
+        self.layers = nn.ModuleList([ResidualBlock(args) for _ in range(args.n_layers)])  
         self.norm = RMSNorm(args.d_model)
         self.lm_head = nn.Linear(args.d_model, args.vocab_size, bias=False)
         self.lm_head.weight = self.embedding.weight
@@ -79,13 +79,89 @@ class Mamba(nn.Module):
         return model
 
 
+class ResidualBlock(nn.Module):
+    def __init__(self, args: ModelArgs):
+        super().__init__()
+        self.args = args
+        self.mixer = MambaBlock(args)
+        self.norm = RMSNorm(args.d_model)
+
+    def forward(self, x):
+        residual = x
+        x = self.norm(x)
+        x = self.mixer(x)
+        return x + residual
+    
 class MambaBlock(nn.Module):
-    pass
+    def __init__(self, args: ModelArgs):
+        super().__init__()
+        self.args = args
+        self.in_proj = nn.Linear(args.d_model, args.d_inner * 2, bias=args.bias)
+        self.conv1d = nn.Conv1d(
+            in_channels=args.d_inner, 
+            out_channels=args.d_inner, 
+            kernel_size=args.d_conv,
+            groups=args.d_inner, 
+            padding=args.d_conv - 1, 
+            bias=args.conv_bias)
+        
+        self.x_proj = nn.Linear(args.d_inner, args.dt_rank + args.d_state * 2, bias=False)
+        self.dt_proj = nn.Linear(args.dt_rank, args.d_inner, bias=True)
+
+        A = repeat(torch.arange(1, args.d_state + 1), 'n -> d n', d=args.d_inner)
+        self.log_A = nn.Parameter(torch.log(A))
+        self.D = nn.Parameter(torch.ones(args.d_inner))
+        self.out_proj = nn.Linear(args.d_inner, args.d_model, bias=args.bias)
+
+    def forward(self, x):
+        (b, d, l) = x.shape
+        x_and_res = self.in_proj(x)
+        (x, res) = x_and_res.split(split_size=[self.args.d_inner, self.args.d_inner], dim=-1)
+        x = rearrange(x, 'b l d_in -> b d_in l')
+        x = self.conv1d(x)[:, :, :l]
+        x = rearrange(x, 'b d_in l -> b l d_in')
+        x = F.silu(x)
+        y = self.ssm(x)
+        y = y * F.silu(res)
+        output = self.out_proj(y)
+        return output
+
+    def ssm(self, x):
+        (d_in, n) = self.A_log.shape
+        A = -torch.exp(self.A_log.float())
+        D = self.D.float()
+        x_dbl = self.x_proj(x)
+        (delta, B, C) = x_dbl.split(split_size=[self.args.dt_rank, n, n], dim=-1)
+        delta = F.softplus(self.dt_proj(delta))
+        y = self.selective_scan(x, delta, A, B, C, D)
+        return y
+    
+    def selective_scan(self, u, delta, A, B, C, D):
+        (b, l, d_in) = u.shape
+        n = A.shape[1]
+        deltaA = torch.exp(einsum(delta, A, 'b l d_in, d_in n -> b l d_in n'))
+        deltaB_u = einsum(delta, B, u, 'b l d_in, b l n, b l d_in -> b l d_in n')
+        x = torch.zeros((b, d_in, n), device=deltaA.device)
+        ys = []    
+        for i in range(l):
+            x = deltaA[:, i] * x + deltaB_u[:, i]
+            y = einsum(x, C[:, i, :], 'b d_in n, b n -> b d_in')
+            ys.append(y)
+        y = torch.stack(ys, dim=1)  # shape (b, l, d_in)
+        
+        y = y + u * D
+    
+        return y
 
 class RMSNorm(nn.Module):
-    pass
+    def __init__(self, d_model, eps=1e-5):
+        super().__init__()
+        self.eps = eps
+        self.scale = nn.Parameter(torch.ones(d_model))  
 
-
+    def forward(self, x):
+        output = x * torch.rsqrt(torch.mean(x ** 2, dim=-1, keepdim=True) + self.eps) * self.scale
+        return output
 
 device = torch.device('mps' if torch.backends.mps.is_available() else 'cpu')
 print('Using device:', device)
